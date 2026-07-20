1709e4a64b2a89cd7d344fffa59b1d09d37286ed
osh9149 <80077374+osh9149@users.noreply.github.com>
2026-07-20 15:18:13 +0900

    Update app.py

--- pop/app.py
+++ pop/app.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 
+DEFAULT_CSV = "202606_202606_연령별인구현황_월간(5).csv"
 
 # =========================================================
 # 기본 설정
